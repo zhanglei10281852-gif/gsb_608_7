597c7dd4acaf685302700696d29001a3efba1b65
zhanglei <czxy408@163.com>
2026-06-10 07:41:51 +0800

    .2517235399659592:81bbda86eef6492faf078b8a26bd126d_6a289853e185ffdee0ac77cb.6a289aafe185ffdee0ac77d1.6a289aae9c7c6380dfc427ed:Trae CN.T(2026/6/10 06:58:55)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,22 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
-    "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier/skip-formatting",
+    "plugin:vue/vue3-recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
     "vue/multi-word-component-names": "off",
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
+  ignorePatterns: ["dist", "node_modules", "docs/.vitepress/dist"],
 };
